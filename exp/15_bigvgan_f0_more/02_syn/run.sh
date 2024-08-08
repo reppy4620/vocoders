227128d545af3965dac598e3939f7d2a41c53924
reppy4620 <ilikeniku@icloud.com>
2024-08-08 23:25:20 +0900

    add exp

--- exp/15_bigvgan_f0_more/02_syn/run.sh
+++ exp/15_bigvgan_f0_more/02_syn/run.sh
@@ -4,7 +4,7 @@ bin_dir=../../../src/vocoders/bin
 
 HYDRA_FULL_ERROR=1 python $bin_dir/synthesize_with_f0.py \
     dataset=with_f0 \
-    generator=f0_aware_bigvgan \
+    generator=f0_aware_bigvgan_more \
     discriminator=bigvgan \
     lit_module=with_f0 \
     syn.ckpt_path=../01_train/out/ckpt/last.ckpt
